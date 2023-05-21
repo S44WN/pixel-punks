8f0fc409a647a90e565886e7a42c42962b117859
Sawan <itissaavn@gmail.com>
2023-05-21 13:09:20 +0530

    add preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,23 +22,33 @@ tl.fromTo(
   }
 );
 
-tl.fromTo(
-  ".hnav__btn",
-  {
-    y: "-100%",
-    opacity: 0,
-  },
-  {
-    duration: 0.5,
-    opacity: 1,
-    y: "0%",
-    stagger: 0.015,
-  }
-);
-
 function togglehm() {
   tl.play();
 }
 function togglec() {
   tl.reverse();
 }
+
+// preloder stuff
+var tlpl = gsap.timeline({ paused: "true" });
+
+tlpl.fromTo(
+  ".preloader",
+  {
+    y: "0%",
+    opacity: 1,
+  },
+  {
+    duration: 2,
+    opacity: 0.5,
+    y: "-100%",
+    stagger: 0.5,
+    ease: Expo.easeInOut,
+  }
+);
+
+const preloder = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  tlpl.play();
+});
